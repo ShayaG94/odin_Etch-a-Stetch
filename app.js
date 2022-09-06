f860a8495f168990b803a5c0324361232a77c6b7
Shaya94 <yginzburg94@gmail.com>
2022-09-07 00:04:01 +0300

    Add range input and functionality

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const container = document.querySelector("#container");
-let grid_size = 50;
+const range = document.querySelector("#gridRange");
 window.addEventListener("resize", () => {});
 
-function createGrid() {
-    let containerSide = container.clientHeight;
-    console.log(container.style.height);
-    [...Array(grid_size ** 2)].forEach(() => {
+range.addEventListener("change", () => {
+    resizeGrid(range.value);
+});
+
+function createGrid(gridSize = 16) {
+    let squareSide = container.clientHeight / gridSize;
+    console.log(container.clientHeight, squareSide);
+    [...Array(gridSize ** 2)].forEach(() => {
         let square = document.createElement("div");
         square.classList.add("border-black");
-        let squareSide = containerSide / grid_size;
         square.style.width = square.style.height = `${squareSide}px`;
         container.append(square);
     });
+}
+
+function resizeGrid(gridSize) {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    createGrid(gridSize);
 }
 
 createGrid();
