8ab84345145aa47fa70b1644a59efb241c8bac7b
Shaya94 <yginzburg94@gmail.com>
2022-09-10 14:10:46 +0300

    Add nav buttons. Add picture mode with pics.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const container = document.querySelector("#container");
 const range = document.querySelector("#gridRange");
 const label = document.querySelector("#num");
 const clearButton = document.querySelector("#clear");
+const photoButton = document.querySelector("#picMode");
+const psychButton = document.querySelector("#psychMode");
 const COLORS = [
     "#D19900",
     "#A60A3D",
@@ -17,6 +19,17 @@ const COLORS = [
     "#4a5973",
     "#007da6",
 ];
+const PHOTOS = [
+    "https://images.unsplash.com/photo-1494548162494-384bba4ab999?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80",
+    "https://images.unsplash.com/photo-1462331940025-496dfbfc7564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=911&q=80",
+    "https://images.unsplash.com/photo-1574169207511-e21a21c8075a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "https://images.unsplash.com/photo-1443926818681-717d074a57af?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "https://images.unsplash.com/photo-1617103539311-b92a6d2c0c1c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "https://images.unsplash.com/photo-1615225150799-524453b31447?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80",
+    "https://images.unsplash.com/photo-1608178398319-48f814d0750c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=879&q=80",
+];
+
+let mode = "psych";
 
 range.addEventListener("input", () => {
     let gridSize = range.value;
@@ -24,9 +37,20 @@ range.addEventListener("input", () => {
     label.innerText = `${gridSize} X ${gridSize}`;
 });
 
-clearButton.addEventListener("click", () => {
-    document.querySelectorAll(".squares").forEach((square) => (square.style.backgroundColor = "white"));
+photoButton.addEventListener("click", () => {
+    resetGrid();
+    mode = "pic";
+    let image = PHOTOS[Math.floor(Math.random() * PHOTOS.length)];
+    container.style.backgroundImage = `url(${image})`;
+    container.style.backgroundSize = "cover";
 });
+
+psychButton.addEventListener("click", () => {
+    resetGrid();
+    mode = "psych";
+});
+
+clearButton.addEventListener("click", resetGrid);
 
 function createGrid(gridSize = range.value) {
     let squareSide = container.clientHeight / gridSize;
@@ -47,14 +71,23 @@ function resizeGrid() {
 }
 
 function changeBgColor(element) {
-    let color = COLORS[Math.floor(Math.random() * COLORS.length)];
-    element.style.backgroundColor = color;
+    if (mode === "psych") {
+        let color = COLORS[Math.floor(Math.random() * COLORS.length)];
+        element.style.backgroundColor = color;
+    }
+    if (mode === "pic") {
+        element.style.backgroundColor = "transparent";
+    }
 }
 
 function clearGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+}
+
+function resetGrid() {
+    document.querySelectorAll(".squares").forEach((square) => (square.style.backgroundColor = "white"));
 }
 
 createGrid();
